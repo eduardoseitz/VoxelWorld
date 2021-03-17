fb922d8eb2c52a0a17f8fd47756fef19b106d36b
Eduardo Seitz <eduardoseitz0808@gmail.com>
2021-03-17 16:29:05 -0300

    Generates Single Quad

---
+++ VOXEL WORLD/Assets/AnatomyOfCube/CubeProperties.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeProperties : MonoBehaviour {
+
+	MeshFilter meshFilter;
+	public Text vertexPrefab;
+	string allVs = "";
+
+	// Use this for initialization
+	void Start () {
+		meshFilter = this.GetComponent<MeshFilter>();
+		
+		GameObject canvas = GameObject.FindWithTag("MainCanvas");
+		Debug.Log("Vertices------");
+		int c = 0;
+		foreach(Vector3 v in meshFilter.mesh.vertices)
+		{
+			Debug.Log(v);
+			string vname = v + "";
+
+			Text vertexLabel;
+			if(!GameObject.Find(vname))
+			{
+				vertexLabel = Instantiate(vertexPrefab, Vector3.zero, Quaternion.identity ) as Text;
+				vertexLabel.gameObject.name = vname;
+        		vertexLabel.transform.SetParent(canvas.transform);
+				vertexLabel.text = v + ": " + c + "";
+				Vector3 vertexLabelPos = Camera.main.WorldToScreenPoint(v + this.transform.position + new Vector3(0,c/80.0f,0));
+            	vertexLabel.transform.position = vertexLabelPos;
+            	
+			}
+			else
+			{
+				vertexLabel = GameObject.Find(vname).GetComponent<Text>();
+				vertexLabel.text += " " + c; 
+			}
+			
+			c++;
+		}
+		
+
+		Debug.Log("Normals-----");
+		foreach(Vector3 n in meshFilter.mesh.normals)
+		{
+			Debug.Log(n);
+			
+		}
+
+		
+		Debug.Log("UVs------");
+		foreach(Vector2 u in meshFilter.mesh.uv)
+		{
+			Debug.Log(u);
+			allVs += u + " ";
+		}
+		
+
+		Debug.Log("Triangles----");
+		foreach(int i in meshFilter.mesh.triangles)
+		{
+			Debug.Log(i);
+			allVs += i + " ";
+		}
+		Debug.Log(allVs);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ VOXEL WORLD/Assets/Scripts/Generator.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    [SerializeField] Material quadMaterial;
+    private Mesh _quadMesh;
+
+    // Mesh data
+    private Vector3[] _vertices;
+    private Vector3[] _normals;
+    private Vector2[] _uvs;
+    private int[] _triangles;
+
+    // UV vectors
+    private Vector2 _uv00;
+    private Vector2 _uv01;
+    private Vector2 _uv10;
+    private Vector2 _uv11;
+
+    // Vertice points vectors
+    private Vector3 _vertice0;
+    private Vector3 _vertice1;
+    private Vector3 _vertice2;
+    private Vector3 _vertice3;
+    private Vector3 _vertice4;
+    private Vector3 _vertice5;
+    private Vector3 _vertice6;
+    private Vector3 _vertice7;
+
+    private GameObject _quadObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateQuad();    
+    }
+
+    private void CreateQuad()
+    {
+        // Create new mesh
+        _quadMesh = new Mesh();
+        _quadMesh.name = "Cube Mesh";
+
+        //all possible UVs
+        _uv00 = new Vector2(0f, 0f);
+        _uv10 = new Vector2(1f, 0f);
+        _uv01 = new Vector2(0f, 1f);
+        _uv11 = new Vector2(1f, 1f);
+
+        //all possible vertices
+        _vertice0 = new Vector3(-0.5f, -0.5f, 0.5f);
+        _vertice1 = new Vector3(0.5f, -0.5f, 0.5f);
+        _vertice2 = new Vector3(0.5f, -0.5f, -0.5f);
+        _vertice3 = new Vector3(-0.5f, -0.5f, -0.5f);
+        _vertice4 = new Vector3(-0.5f, 0.5f, 0.5f);
+        _vertice5 = new Vector3(0.5f, 0.5f, 0.5f);
+        _vertice6 = new Vector3(0.5f, 0.5f, -0.5f);
+        _vertice7 = new Vector3(-0.5f, 0.5f, -0.5f);
+
+        // Construct data to be passed to mesh
+        _vertices = new Vector3[] { _vertice4, _vertice5, _vertice1, _vertice0 };
+        _normals = new Vector3[] { Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward };
+        _uvs = new Vector2[] { _uv11, _uv01, _uv00, _uv10 };
+        _triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+
+        // Put data into the mesh itself
+        _quadMesh.vertices = _vertices;
+        _quadMesh.normals = _normals;
+        _quadMesh.uv = _uvs;
+        _quadMesh.triangles = _triangles;
+
+        // Recalculate bounding box for rendering so that it is ocluded correctly
+        _quadMesh.RecalculateBounds();
+
+        _quadObject = new GameObject("Quad");
+        _quadObject.transform.parent = transform;
+        _quadObject.AddComponent<MeshFilter>().mesh = _quadMesh;
+        _quadObject.AddComponent<MeshRenderer>().material = quadMaterial;
+    }
+}
