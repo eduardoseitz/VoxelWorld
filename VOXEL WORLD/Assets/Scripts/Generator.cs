1c02b34b695514085c31470876d2988e8913e2bc
Eduardo Seitz <eduardoseitz0808@gmail.com>
2021-03-17 16:30:34 -0300

    Generates a cube

--- VOXEL WORLD/Assets/Scripts/Generator.cs
+++ VOXEL WORLD/Assets/Scripts/Generator.cs
@@ -34,22 +34,24 @@ public class Generator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CreateQuad();    
+        CreateCube();
     }
 
-    private void CreateQuad()
+    private void CreateCube()
     {
-        // Create new mesh
-        _quadMesh = new Mesh();
-        _quadMesh.name = "Cube Mesh";
+        // Create data required by mesh filter
+        _vertices = new Vector3[4];
+        _normals = new Vector3[4];
+        _uvs = new Vector2[4];
+        _triangles = new int[6];
 
-        //all possible UVs
+        // All possible UVs
         _uv00 = new Vector2(0f, 0f);
         _uv10 = new Vector2(1f, 0f);
         _uv01 = new Vector2(0f, 1f);
         _uv11 = new Vector2(1f, 1f);
 
-        //all possible vertices
+        // All possible vertices
         _vertice0 = new Vector3(-0.5f, -0.5f, 0.5f);
         _vertice1 = new Vector3(0.5f, -0.5f, 0.5f);
         _vertice2 = new Vector3(0.5f, -0.5f, -0.5f);
@@ -59,9 +61,48 @@ public class Generator : MonoBehaviour
         _vertice6 = new Vector3(0.5f, 0.5f, -0.5f);
         _vertice7 = new Vector3(-0.5f, 0.5f, -0.5f);
 
+        // Generate each side of the cube
+        CreateQuad(CubeSide.Front);
+        CreateQuad(CubeSide.Back);
+        CreateQuad(CubeSide.Right);
+        CreateQuad(CubeSide.Left);
+        CreateQuad(CubeSide.Top);
+        CreateQuad(CubeSide.Bottom);
+    }
+
+    private void CreateQuad(CubeSide cubeSide)
+    {
+        // Create new mesh
+        _quadMesh = new Mesh();
+
         // Construct data to be passed to mesh
-        _vertices = new Vector3[] { _vertice4, _vertice5, _vertice1, _vertice0 };
-        _normals = new Vector3[] { Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward };
+        switch (cubeSide)
+        {
+            case CubeSide.Bottom:
+                _vertices = new Vector3[] { _vertice0, _vertice1, _vertice2, _vertice3 };
+                _normals = new Vector3[] { Vector3.down, Vector3.down, Vector3.down, Vector3.down };
+                break;
+            case CubeSide.Top:
+                _vertices = new Vector3[] { _vertice7, _vertice6, _vertice5, _vertice4 };
+                _normals = new Vector3[] { Vector3.up, Vector3.up, Vector3.up, Vector3.up };
+                break;
+            case CubeSide.Left:
+                _vertices = new Vector3[] { _vertice7, _vertice4, _vertice0, _vertice3 };
+                _normals = new Vector3[] { Vector3.left, Vector3.left, Vector3.left, Vector3.left };
+                break;
+            case CubeSide.Right:
+                _vertices = new Vector3[] { _vertice5, _vertice6, _vertice2, _vertice1 };
+                _normals = new Vector3[] { Vector3.right, Vector3.right, Vector3.right, Vector3.right };
+                break;
+            case CubeSide.Front:
+                _vertices = new Vector3[] { _vertice4, _vertice5, _vertice1, _vertice0 };
+                _normals = new Vector3[] { Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward };
+                break;
+            case CubeSide.Back:
+                _vertices = new Vector3[] { _vertice6, _vertice7, _vertice3, _vertice2 };
+                _normals = new Vector3[] { Vector3.back, Vector3.back, Vector3.back, Vector3.back };
+                break;
+        }
         _uvs = new Vector2[] { _uv11, _uv01, _uv00, _uv10 };
         _triangles = new int[] { 3, 1, 0, 3, 2, 1 };
 
@@ -74,9 +115,11 @@ public class Generator : MonoBehaviour
         // Recalculate bounding box for rendering so that it is ocluded correctly
         _quadMesh.RecalculateBounds();
 
-        _quadObject = new GameObject("Quad");
+        _quadObject = new GameObject("QuadMesh " + cubeSide.ToString());
         _quadObject.transform.parent = transform;
         _quadObject.AddComponent<MeshFilter>().mesh = _quadMesh;
         _quadObject.AddComponent<MeshRenderer>().material = quadMaterial;
     }
 }
+
+public enum CubeSide { Top, Bottom, Front, Back, Right, Left }
