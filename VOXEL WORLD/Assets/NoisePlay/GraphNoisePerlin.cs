877ff8ef2eb701bff4ffb2411e3da042194b75de
Eduardo Seitz <eduardoseitz0808@gmail.com>
2021-03-22 20:25:49 -0300

    Noise Graph Tests

---
+++ VOXEL WORLD/Assets/NoisePlay/GraphNoisePerlin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphNoisePerlin : MonoBehaviour 
+{
+	float time = 0;
+    float highFrequency = 0.01f; // Higher frequency is more plain
+
+    float time1 = 0;
+    float lowFrequecy = 0.001f; // Short frequency is more plain
+
+    float time2; // Combining both and we get a frequency that has both planices and hills
+
+    void Update () 
+	{
+        time += highFrequency;
+        float yellowGraph = Mathf.PerlinNoise(time, 1);
+        //Grapher.Log(yellowGraph, "ShortFrequencyPerlin", Color.yellow);
+
+        time1 += lowFrequecy;
+        float greenGraph = Mathf.PerlinNoise(time1, 1);
+        //Grapher.Log(greenGraph, "HighFrequencyPerlin", Color.green);
+
+        time2 = time + time1; // Combine the 2 frequencies
+        time2 = time / 2; // Average the value to normalized it
+        float redGraph = CovertFrequencyScale(0, 32, 0, 1, time2); // Change frequecy range from 0 up to 1 to 0 up to 32
+        Grapher.Log(redGraph, "CombinedFrequencyPerlin", Color.red);
+    }
+
+    float CovertFrequencyScale(float newMin, float newMax, float oldMin, float oldMax, float value)
+    {
+        return Mathf.Lerp(newMin, newMax, Mathf.InverseLerp(oldMin, oldMax, value));
+    }
+}
---
+++ VOXEL WORLD/Assets/NoisePlay/Grapher/GenTerrain.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenTerrain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+	
+		Vector3[] vertices = new Vector3[mesh.vertices.Length];
+		
+		for (var i=0;i<vertices.Length;i++)
+		{
+			vertices[i] = new Vector3(mesh.vertices[i].x, 
+										Mathf.PerlinNoise(mesh.vertices[i].x,mesh.vertices[i].z)*3,
+										mesh.vertices[i].z);
+			
+		}
+
+
+		mesh.vertices = vertices;
+		mesh.RecalculateNormals();
+		mesh.RecalculateBounds();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
